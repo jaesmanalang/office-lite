c314eca7f73cd5421093a6888063bda9572ce9a6
James Manalang <james.e.manalang@gmail.com>
2022-05-04 22:02:57 +0800

    Update reponsive design for desktop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import headerBgPattern from './assets/home/bg-pattern-header.svg';
 import headerLogo from './assets/shared/logo.svg';
 import heroImg from './assets/home/illustration-charts.svg';
+import footerBgPattern from './assets/home/bg-pattern-footer.svg';
 // import pricingCardBg from './assets/home/bg-pattern-pricing.svg';
 import './scss/main.scss';
 
 document.querySelector('.header__bg-pattern').src = headerBgPattern;
 document.querySelector('.header__logo').src = headerLogo;
 document.querySelector('.hero__img').src = heroImg;
+document.querySelector('.footer__bg-pattern').src = footerBgPattern;
 // const pricingCardBgPattern = new Image();
 // pricingCardBgPattern.src = pricingCardBg;
 // document.body.appendChild(pricingCardBgPattern);
